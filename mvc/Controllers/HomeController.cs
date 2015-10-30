9c196e7d1f105b0532a1a692417b6af9d8070ee0
ArturScholz <arturus92@gmail.com>
2015-10-31 00:53:42 +0100

    fajny komentarz

--- mvc/Controllers/HomeController.cs
+++ mvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mvc.Models;
 
 namespace mvc.Controllers
 {
@@ -11,22 +12,30 @@ namespace mvc.Controllers
         //
         // GET: /Home/
         public string test = "testowy napis";
-        public ActionResult Index()
+        public ActionResult Index() // poczatkowa strona
         {
             int hour = DateTime.Now.Hour;
 
             if (hour < 17)
-                ViewBag.Message = "goood moornig vieeetnaaam!";
+                ViewBag.Message = "goood moorning vieeetnaaam!";
             else
-                ViewBag.Message = "fuck u too";
+                ViewBag.Message = "other greeting";
             
             return View();
         }
 
-        public ViewResult RsvpForm()
+        [HttpGet] //get
+        public ViewResult RsvpForm() // formularz który jest dostepny po klikneciu liku rsvpForm w na glownej stronie - polaczenie przez nazwe
         {
-            return View();
+            return View(); // odpala widok RspForm
         }
 
+        
+        [HttpPost] //post == set
+        public ViewResult RsvpForm(GuestResponse guestResponse) //zapisane dane sa przechwytywane przez ta metode
+        {
+            return View("Thanks", guestResponse); // zbiera dane wpisane w formularz i wprowadza do obiektu guestResponse
+                                                  // THANKS to nazwa widoku! zajebiste!
+        }
     }
 }
--- mvc/Models/GuestResponse.cs
+++ mvc/Models/GuestResponse.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace mvc.Models
 {
     public class GuestResponse
     {
+       
+        [Required(AllowEmptyStrings = false, ErrorMessage = "podaj imie i nazwisko:", ErrorMessageResourceName = "Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage="podaj email")]
+       // [EmailAddress]
         public string Email { get; set; }
+        [Required()]
         public string Phone { get; set; }
-        public bool WillAttend { get; set; }
+        public bool? WillAttend { get; set; }
 
     }
 }
